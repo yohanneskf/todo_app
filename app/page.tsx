a5d1f0d88e9e6fee4852349858063fc326591d59
yohanneskf <yohanneskifle540@gmail.com>
2025-09-14 22:37:54 +0300

    bc

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import DemoPage from "./lists/page";
+import { columns, Todo } from "./columns";
+import { DataTable } from "./data-table";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-  return <DemoPage />;
+async function getData(): Promise<Todo[]> {
+  // Fetch data from your API here.
+
+  return await prisma.todo.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+    },
+  });
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
