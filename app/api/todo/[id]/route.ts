46c5f859438c3d85ff5bbdba856c75ff867e6dd2
yohanneskf <yohanneskifle540@gmail.com>
2025-09-14 15:46:27 +0300

    s

--- app/api/todo/[id]/route.ts
+++ app/api/todo/[id]/route.ts
@@ -1,34 +1,61 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
+// DELETE handler to delete a ToDo item by its ID.
 export async function DELETE(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = params;
-    await prisma.todo.delete({ where: { id: Number(id) } });
-    return NextResponse.json({ success: true });
+    const deletedTodo = await prisma.todo.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    if (!deletedTodo) {
+      return NextResponse.json(
+        { error: "ToDo item not found." },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(deletedTodo, { status: 200 });
   } catch (error) {
-    console.error("Failed to delete todo:", error);
-    return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to delete ToDo item." },
+      { status: 500 }
+    );
   }
 }
 
+// PUT handler to update a ToDo item by its ID.
 export async function PUT(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = params;
     const body = await request.json();
+    const { title, description } = body;
+    if (!title) {
+      return NextResponse.json(
+        { error: "Title is required." },
+        { status: 400 }
+      );
+    }
     const updatedTodo = await prisma.todo.update({
-      where: { id: Number(id) },
-      data: body,
+      where: {
+        id: Number(params.id),
+      },
+      data: {
+        title,
+        description,
+      },
     });
-    return NextResponse.json(updatedTodo);
+    return NextResponse.json(updatedTodo, { status: 200 });
   } catch (error) {
-    console.error("Failed to update todo:", error);
-    return NextResponse.json({ error: "Failed to update" }, { status: 500 });
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed to update ToDo item." },
+      { status: 500 }
+    );
   }
 }
